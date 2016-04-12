6c1acce2bd58b8c04f75da43ce59d32eeac0e8d5
Ilya Razenshteyn <ilyaraz@mit.edu>
2016-04-12 13:59:45 -0400

    docstrings

--- ffht/__init__.py
+++ ffht/__init__.py
@@ -1,7 +1,26 @@
+"""Python wrapper for FFHT.
+
+This is a Python wrapper for [FFHT](https://github.com/FALCONN-LIB/FFHT).
+It exposes two functions: `create_aligned`, which creates aligned
+one-dimensional [NumPy](http://www.numpy.org/) arrays, and `fht`, which
+performs the Fast Hadamard Transform on a given one-dimensional NumPy
+array.
+
+To the best of our knowledge, FFHT is currently the fastest
+implementation of *any* Fourier-like transform.
+"""
+
 from _ffht import fht
 import numpy as _numpy
 
 def create_aligned(n, dtype, alignment=32):
+    """ Create an aligned one-dimensional NumPy array.
+
+    Creates a one-dimensional NumPy array of length `n`
+    with a given `dtype` that is aligned to `alignment` bytes.
+
+    NB: `alignment` must be a multiple of the size of `dtype`.
+    """
     buf = _numpy.zeros(n + alignment, dtype=dtype)
     if alignment % buf.itemsize:
         raise ValueError('alignment must be a multiple of the size of dtype')
