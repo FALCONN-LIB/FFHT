6897d3f5d764db961bba025cbf63a1e906ca71b1
Ilya Razenshteyn <ilyaraz@mit.edu>
2016-05-02 09:43:22 -0400

    code formatting

--- _ffht.c
+++ _ffht.c
@@ -5,39 +5,46 @@
 #define UNUSED(x) (void)(x)
 
 static char module_docstring[] =
-  "A C extension that computes the Fast Hadamard Transform";
+    "A C extension that computes the Fast Hadamard Transform";
 static char fht_docstring[] =
-"Compute the Fast Hadamard Transform (FHT) for a given "
-"one-dimensional NumPy array.\n\n"
-"The Hadamard Transform is a linear orthogonal map defined on real vectors "
-"whose length is a _power of two_. For the precise definition, see the "
-"[Wikipedia entry](https://en.wikipedia.org/wiki/Hadamard_transform). The "
-"Hadamard Transform has been recently used a lot in various machine learning "
-"and numerical algorithms.\n\n"
-"The implementation uses "
-"[AVX](https://en.wikipedia.org/wiki/Advanced_Vector_Extensions) "
-"to speed up the computation. If AVX is not supported on your machine, "
-"a simpler implementation without (explicit) vectorization is used.\n\n"
-"The function takes two parameters:\n\n"
-"* `buffer` is a NumPy array which is being transformed. It must be a "
-"one-dimensional array with `dtype` equal to `float32` or `float64` (the "
-"former is recommended unless you need high accuracy) and of size being a power "
-"of two. If your CPU supports AVX, then `buffer` must be aligned to 32 bytes. "
-"To allocate such an aligned buffer, use the function `created_aligned` from this "
-"module.\n"
-"* `chunk` is a positive integer that controls when the implementation switches "
-"from recursive to iterative algorithm. The overall algorithm is recursive, but as "
-"soon as the vector becomes no longer than `chunk`, the iterative algorithm is "
-"invoked. For technical reasons, `chunk` must be at least 8. A good choice is to "
-"set `chunk` to 1024. But to fine-tune the performance one should use a program "
-"`best_chunk` supplied with the library.\n";
+    "Compute the Fast Hadamard Transform (FHT) for a given "
+    "one-dimensional NumPy array.\n\n"
+    "The Hadamard Transform is a linear orthogonal map defined on real vectors "
+    "whose length is a _power of two_. For the precise definition, see the "
+    "[Wikipedia entry](https://en.wikipedia.org/wiki/Hadamard_transform). The "
+    "Hadamard Transform has been recently used a lot in various machine "
+    "learning "
+    "and numerical algorithms.\n\n"
+    "The implementation uses "
+    "[AVX](https://en.wikipedia.org/wiki/Advanced_Vector_Extensions) "
+    "to speed up the computation. If AVX is not supported on your machine, "
+    "a simpler implementation without (explicit) vectorization is used.\n\n"
+    "The function takes two parameters:\n\n"
+    "* `buffer` is a NumPy array which is being transformed. It must be a "
+    "one-dimensional array with `dtype` equal to `float32` or `float64` (the "
+    "former is recommended unless you need high accuracy) and of size being a "
+    "power "
+    "of two. If your CPU supports AVX, then `buffer` must be aligned to 32 "
+    "bytes. "
+    "To allocate such an aligned buffer, use the function `created_aligned` "
+    "from this "
+    "module.\n"
+    "* `chunk` is a positive integer that controls when the implementation "
+    "switches "
+    "from recursive to iterative algorithm. The overall algorithm is "
+    "recursive, but as "
+    "soon as the vector becomes no longer than `chunk`, the iterative "
+    "algorithm is "
+    "invoked. For technical reasons, `chunk` must be at least 8. A good choice "
+    "is to "
+    "set `chunk` to 1024. But to fine-tune the performance one should use a "
+    "program "
+    "`best_chunk` supplied with the library.\n";
 
 static PyObject *ffht_fht(PyObject *self, PyObject *args);
 
 static PyMethodDef module_methods[] = {
-  {"fht", ffht_fht, METH_VARARGS, fht_docstring},
-  {NULL, NULL, 0, NULL}
-};
+    {"fht", ffht_fht, METH_VARARGS, fht_docstring}, {NULL, NULL, 0, NULL}};
 
 PyMODINIT_FUNC init_ffht(void);
 
@@ -49,12 +56,11 @@ PyMODINIT_FUNC init_ffht(void) {
 }
 
 static PyObject *ffht_fht(PyObject *self, PyObject *args) {
-
   UNUSED(self);
 
   PyObject *buffer_obj;
   int chunk_size;
-  
+
   if (!PyArg_ParseTuple(args, "Oi", &buffer_obj, &chunk_size)) {
     return NULL;
   }
@@ -68,8 +74,9 @@ static PyObject *ffht_fht(PyObject *self, PyObject *args) {
   int ndim;
   npy_intp dims[NPY_MAXDIMS];
   PyArrayObject *arr = NULL;
-  
-  if (PyArray_GetArrayParamsFromObject(buffer_obj, NULL, 1, &dtype, &ndim, dims, &arr, NULL) < 0) {
+
+  if (PyArray_GetArrayParamsFromObject(buffer_obj, NULL, 1, &dtype, &ndim, dims,
+                                       &arr, NULL) < 0) {
     return NULL;
   }
 
@@ -110,11 +117,10 @@ static PyObject *ffht_fht(PyObject *self, PyObject *args) {
 #endif
   int res;
   if (dtype->type_num == NPY_FLOAT) {
-    float *buffer = (float*)raw_buffer;
+    float *buffer = (float *)raw_buffer;
     res = FHTFloat(buffer, n, chunk_size);
-  }
-  else {
-    double *buffer = (double*)raw_buffer;
+  } else {
+    double *buffer = (double *)raw_buffer;
     res = FHTDouble(buffer, n, chunk_size);
   }
 
