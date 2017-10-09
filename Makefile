0bd7941d6ac10bfd9c8c1dec41c849cda7c9e64a
dnbh <dnb@cs.jhu.edu>
2017-10-09 18:08:54 -0400

    Add fast_copy: unrolled vectorized memory copy instructions.

--- Makefile
+++ Makefile
@@ -4,8 +4,9 @@ CFLAGS = -O3 -march=native -std=$(STD) -pedantic -Wall -Wextra -Wshadow -Wpointe
 
 all:
 	$(CC) fht.c -c $(CFLAGS)
-	$(CC) test_float.c fht.o -o test_float $(CFLAGS)
-	$(CC) test_double.c fht.o -o test_double $(CFLAGS)
+	$(CC) fast_copy.c -c $(CFLAGS)
+	$(CC) test_float.c fast_copy.o fht.o -o test_float $(CFLAGS)
+	$(CC) test_double.c fast_copy.o fht.o -o test_double $(CFLAGS)
 
 clean:
 	rm -f fht.o test_float test_double
--- fht_impl.h
+++ fht_impl.h
@@ -1,4 +1,4 @@
-#include <string.h>
+#include "fast_copy.h"
 #ifdef __AVX__
 #include "fht_avx.c"
 #define VECTOR_WIDTH (32u)
@@ -8,12 +8,12 @@
 #endif
 
 int fht_float_oop(float *in, float *out, int log_n) {
-    memcpy(out, in, sizeof(float) << log_n);
+    fast_copy(out, in, sizeof(float) << log_n);
     return fht_float(out, log_n);
 }
 
 int fht_double_oop(double *in, double *out, int log_n) {
-    memcpy(out, in, sizeof(double) << log_n);
+    fast_copy(out, in, sizeof(double) << log_n);
     return fht_double(out, log_n);
 }
 
