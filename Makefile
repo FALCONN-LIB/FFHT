58b63893ea1862442cc30971d26b6820b96d8cfa
dnbh <dnb@cs.jhu.edu>
2017-10-10 15:42:47 -0400

    Simplify fast_copy. It is ~10% faster than gcc's memcpy.

--- Makefile
+++ Makefile
@@ -1,11 +1,19 @@
 CC = gcc
 CFLAGS = -O3 -march=native -std=c99 -pedantic -Wall -Wextra -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes -Wmissing-prototypes
 
-all:
+fht.o: fht.c
 	$(CC) fht.c -c $(CFLAGS)
+
+fast_copy.o: fast_copy.c
 	$(CC) fast_copy.c -c $(CFLAGS)
+
+test_float: test_float.c fast_copy.o fht.c
 	$(CC) test_float.c fast_copy.o fht.o -o test_float $(CFLAGS)
+
+test_double: test_double.c fast_copy.o fht.c
 	$(CC) test_double.c fast_copy.o fht.o -o test_double $(CFLAGS)
+
+all: test_float test_double fast_copy.o fht.o
 
 clean:
 	rm -f fht.o test_float test_double fast_copy.o
--- fast_copy.c
+++ fast_copy.c
@@ -16,20 +16,7 @@ void fast_copy(void *out, void *in, size_t n) {
     }
     __m512 *ov = (__m512 *)out;
     __m512 *iv = (__m512 *)in;
-    size_t f = (n >> 6) >> 3, leftover = (n >> 6) & 0x7u;
-    n = 0;
-    switch(leftover) {
-        case 0: do {
-                ov[n] = iv[n]; ++n;
-        case 7: ov[n] = iv[n]; ++n;
-        case 6: ov[n] = iv[n]; ++n;
-        case 5: ov[n] = iv[n]; ++n;
-        case 4: ov[n] = iv[n]; ++n;
-        case 3: ov[n] = iv[n]; ++n;
-        case 2: ov[n] = iv[n]; ++n;
-        case 1: ov[n] = iv[n]; ++n;
-                   } while(f--);
-    }
+    for(n >>= 6; n--; ov[n] = iv[n]);
 }
 #elif __AVX2__
 // If unaligned or n is less than 32, defaults to memcpy.
@@ -40,20 +27,7 @@ void fast_copy(void *out, void *in, size_t n) {
     }
     __m256 *ov = (__m256 *)out;
     __m256 *iv = (__m256 *)in;
-    size_t f = (n >> 5) >> 3, leftover = (n >> 5) & 0x7u;
-    n = 0;
-    switch(leftover) {
-        case 0: do {
-                ov[n] = iv[n]; ++n;
-        case 7: ov[n] = iv[n]; ++n;
-        case 6: ov[n] = iv[n]; ++n;
-        case 5: ov[n] = iv[n]; ++n;
-        case 4: ov[n] = iv[n]; ++n;
-        case 3: ov[n] = iv[n]; ++n;
-        case 2: ov[n] = iv[n]; ++n;
-        case 1: ov[n] = iv[n]; ++n;
-                   } while(f--);
-    }
+    for(n >>= 5; n--; ov[n] = iv[n]);
 }
 #elif __SSE2__
 // If unaligned or n is less than 16, defaults to memcpy
@@ -65,20 +39,7 @@ void fast_copy(void *out, void *in, size_t n) {
     }
     __m128 *ov = (__m128 *)out;
     __m128 *iv = (__m128 *)in;
-    size_t f = (n >> 4) >> 3, leftover = (n >> 4) & 0x7u;
-    n = 0;
-    switch(leftover) {
-        case 0: do {
-                ov[n] = iv[n]; ++n;
-        case 7: ov[n] = iv[n]; ++n;
-        case 6: ov[n] = iv[n]; ++n;
-        case 5: ov[n] = iv[n]; ++n;
-        case 4: ov[n] = iv[n]; ++n;
-        case 3: ov[n] = iv[n]; ++n;
-        case 2: ov[n] = iv[n]; ++n;
-        case 1: ov[n] = iv[n]; ++n;
-                   } while(f--);
-    }
+    for(n >>= 4; n--; ov[n] = iv[n]);
 }
 #else
 void fast_copy(void *out, void *in, size_t n) {
